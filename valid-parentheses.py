28991224b461c7a2965cacb487a120002df71a0b
Noor Alkhateeb <109729376+Noor696@users.noreply.github.com>
2023-08-10 10:00:27 +0300

    Update valid-parentheses.py

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -15,3 +15,28 @@ class Solution:
                 return False
     
         return len(stack) == 0
+
+# Another way by using pop & append
+def isValidParentheses(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}  # Mapping for closing and opening brackets
+
+    for char in s:
+        if char in mapping.values():  # If it's an opening bracket, push it onto the stack
+            stack.append(char)
+        elif char in mapping.keys():   # If it's a closing bracket
+            if stack and stack[-1] == mapping[char]:  # Check if the stack is not empty and matches the opening bracket
+                stack.pop()
+            else:
+                return False
+        else:
+            return False  # If the character is not a bracket, return False
+    
+    return not stack  # Return True if the stack is empty, False otherwise
+
+# Test cases
+print(isValidParentheses("()"))  # True
+print(isValidParentheses("()[]{}"))  # True
+print(isValidParentheses("(]"))  # False
+print(isValidParentheses("([)]"))  # False
+print(isValidParentheses("{[]}"))  # True
